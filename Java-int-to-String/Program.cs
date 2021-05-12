796972637a5c525da276337b0e9dd56752bd12b5
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-12 01:56:40 -0500

    Agregar archivos de proyecto.

---
+++ Java-int-to-String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Java_int_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame un numero");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if  (n> -100 && n< 100) { Console.WriteLine("Buen trabajo"); }
+            else { Console.WriteLine("Respuesta equivocada"); }
+        }
+    }
+}
